a570236ff069c0a9f999718507d762bf370a851e
Lamapotato <agranat42@gmail.com>
2026-07-27 21:29:55 +0300

    43. Делаем инвентарь

---
+++ Source/UME/Private/InventoryComponent.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryComponent.h"
+
+// Sets default values for this component's properties
+UInventoryComponent::UInventoryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+bool UInventoryComponent::SwapBetween(UInventoryComponent* CompA, int32 IndexA,
+	UInventoryComponent* CompB, int32 IndexB)
+{
+	if (!CompA || !CompB) return false;
+
+
+	const int32 SizeA = CompA->Slots.Num();
+	const int32 SizeB = CompB->Slots.Num();
+
+	if (!IsValidIndex(IndexA, SizeA) || !IsValidIndex(IndexB, SizeB))
+	{
+#if WITH_EDITOR
+		UE_LOG(LogTemp, Warning,
+			TEXT("UInventoryComponent::SwapBetween: invalid indices (%d,%d) for sizes %d/%d"),
+			IndexA, IndexB, SizeA, SizeB);
+#endif
+		return false;
+	}
+
+	// Сохраняем временно элемент из первого инвентаря
+	FSlotInfo Temp = CompA->Slots[IndexA];
+
+	// Переносим второй в первый
+	CompA->Slots[IndexA] = CompB->Slots[IndexB];
+	// И первый во второй
+	CompB->Slots[IndexB] = Temp;
+
+#if WITH_EDITOR
+	UE_LOG(LogTemp, Log,
+		TEXT("UInventoryComponent::SwapBetween: swapped [%s:%d] <-> [%s:%d]"),
+		*CompA->GetName(), IndexA, *CompB->GetName(), IndexB);
+#endif
+
+	return true;
+}
+bool UInventoryComponent::IsValidIndex(int32 Idx, int32 Size)
+{
+	return (Idx >= 0 && Idx < Size);
+}
---
+++ Source/UME/public/InventoryComponent.h
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSlotInfo
+{
+	GENERATED_BODY()
+
+public:
+	/** Class type for objects that can occupy this slot. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	TSubclassOf<AActor> ObjectClass;
+
+	/** Current number of objects in the slot (or max allowed). */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	int32 ObjectCount = 0;
+
+};
+
+USTRUCT(BlueprintType)
+struct FSlotUIInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	/** Human‑readable name shown in UI. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	FString DisplayName;
+
+	/** Longer description or tooltip text. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	FString Description;
+
+	/** Icon that will be displayed next to the entry. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	UTexture2D* Icon;
+};
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UME_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInventoryComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TArray<FSlotInfo> Slots;
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory|Utility")
+	bool SwapBetween(UInventoryComponent* CompA, int32 IndexA,
+		UInventoryComponent* CompB, int32 IndexB);
+private:
+	bool IsValidIndex(int32 Idx, int32 Size);
+};
